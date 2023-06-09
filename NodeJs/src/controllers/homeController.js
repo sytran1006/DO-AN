57bf3458e5682ecbfcfcdc545d2bab93bcb77d8e
TRAN VAN SY 20182763 <sy.tv182763@sis.hust.edu.vn>
2023-06-09 13:58:32 +0700

    ket noi db

--- NodeJs/src/controllers/homeController.js
+++ NodeJs/src/controllers/homeController.js
@@ -1,5 +1,17 @@
-let getHomePage = (req, res) =>{
-    return res.render("homePage.ejs");
+import db from "../models/index";
+
+let getHomePage = async(req, res) =>{
+    try{
+        let data = await db.User.findAll();
+        console.log('----------------')
+        console.log(data)
+        console.log('----------------')
+        return res.render("homePage.ejs", {
+            data: JSON.stringify(data)
+        });
+    } catch(e){
+        console.log(e);
+    }
 
 }
 let getCartPage = (req, res) =>{
